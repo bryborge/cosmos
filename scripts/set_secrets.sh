aac9e446f4db4070086cf6cb07e6429742246708
Bryan Borgeson <sonofborge@protonmail.com>
2022-07-23 19:37:54 -0700

    move over to using env vars for secrets w/ pass

--- scripts/set_secrets.sh
+++ scripts/set_secrets.sh
@@ -1,8 +1,13 @@
-
 #!/usr/bin/env bash
 
 set -euo pipefail
 
+# Packer
+export PKR_VAR_proxmox_api_url=$(pass homelab/proxmox/api_url)
+export PKR_VAR_proxmox_api_packer_token_id=$(pass homelab/proxmox/pkr_token_id)
+export PKR_VAR_proxmox_api_packer_token_secret=$(pass homelab/proxmox/pkr_token_secret)
+
+# Terraform
 export TF_VAR_primary_user_password=$(pass homelab/dockserv/xsob_password)
 export TF_VAR_proxmox_api_url=$(pass homelab/proxmox/api_url)
 export TF_VAR_proxmox_api_terraform_token_id=$(pass homelab/proxmox/tf_token_id)
