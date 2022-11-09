892e3cb20f829f522f68a6ab655fd2604707e887
Bryan Borgeson <sonofborge@protonmail.com>
2022-11-08 22:13:20 -0800

    get it into a 'working' state

--- infrastructure/homelab/proxmox/dockserv/main.tf
+++ infrastructure/homelab/proxmox/dockserv/main.tf
@@ -10,22 +10,22 @@ resource "proxmox_vm_qemu" "dockserv" {
   name  = "dockserv-${count.index + 1}"
 
   target_node = var.proxmox_node
-  clone       = "xsob-ubuntu-server-jammy-v1.0.5"
+  clone       = "xsob-ubuntu-server-jammy-v1.0.0"
 
   agent    = 1
   bootdisk = "scsi0"
   cores    = 2
   cpu      = "host"
   os_type  = "cloud-init"
-  memory   = 8192
+  memory   = 4096
   scsihw   = "virtio-scsi-pci"
   sockets  = 1
 
   disk {
-    iothread     = 1
-    size         = "250G"
-    storage      = "tank"
-    type         = "scsi"
+    iothread = 1
+    size     = "100G"
+    storage  = "tank"
+    type     = "scsi"
   }
 
   network {
@@ -43,21 +43,7 @@ resource "proxmox_vm_qemu" "dockserv" {
     type     = "ssh"
     user     = "xsob"
     password = var.user_password
-    host     = "192.168.1.91"
+    host     = "192.168.1.9${count.index + 1}"
     port     = "22"
-  }
-
-  provisioner "remote-exec" {
-    # TODO: Break these out into provisioner scripts
-    inline = [
-      # 1. clone the dockserv project
-      "git clone https://github.com/sonofborge/dockserv.git /home/xsob/dockserv && cd $_ && git checkout main",
-      # 2. create nas media mount point
-      "echo '${var.user_password}' | sudo -S mkdir -p /media/nas",
-      "echo '${var.nas_ip}:<PATH/TO/SHARE> /media/nas nfs auto,defaults,nofail 0 0' > /etc/fstab", # FIXME
-      "mount -a",
-      # 3. set hostname
-      "sudo hostnamectl set-hostname dockserv-${count.index + 1}",
-    ]
   }
 }
