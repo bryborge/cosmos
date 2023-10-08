94d652847d443d939e5fb5718da602d0c85fb784
Bryan Borgeson <bryborge@gmail.com>
2023-10-07 22:13:42 -0700

    spacing

--- aws/modules/s3-bucket-site/cloudfront.tf
+++ aws/modules/s3-bucket-site/cloudfront.tf
@@ -32,6 +32,7 @@ resource "aws_cloudfront_distribution" "cloudfront" {
 
     forwarded_values {
       query_string = true
+
       cookies {
         forward = "none"
       }
--- aws/modules/s3-bucket-site/data.tf
+++ aws/modules/s3-bucket-site/data.tf
@@ -4,10 +4,12 @@ data "aws_iam_policy_document" "website_policy" {
       "s3:GetObject",
       "s3:ListBucket"
     ]
+
     principals {
       identifiers = [aws_cloudfront_origin_access_identity.oai.iam_arn]
       type        = "AWS"
     }
+
     resources = [
       "arn:aws:s3:::${var.domain_name}/*",
       "arn:aws:s3:::${var.domain_name}"
