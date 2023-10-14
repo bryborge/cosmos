c3b090cf0f0228eb45a9fdd540daa42dcee57fa0
Bryan Borgeson <sonofborge@protonmail.com>
2023-10-14 01:50:33 -0700

    Add aws iam user and policy for cicd_deployer for the blog (#28)

--- aws/modules/s3-bucket-site/iam.tf
+++ aws/modules/s3-bucket-site/iam.tf
@@ -4,10 +4,12 @@ data "aws_iam_policy_document" "website_policy" {
       "s3:GetObject",
       "s3:ListBucket"
     ]
+
     principals {
       type        = "AWS"
       identifiers = ["*"]
     }
+
     resources = [
       "arn:aws:s3:::${var.bucket_name}/*",
       "arn:aws:s3:::${var.bucket_name}"
