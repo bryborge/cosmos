773e017cd006589a0097066c0afd66a8b8d4a2ad
Bryan Borgeson <bryborge@gmail.com>
2023-10-14 01:49:11 -0700

    add aws iam user and policy for cicd_deployer for the blog

--- aws/live/production/blog/main.tf
+++ aws/live/production/blog/main.tf
@@ -19,3 +19,28 @@ module "s3_bucket_site" {
   index_html     = var.index_html
   error_html     = var.error_html
 }
+
+data "aws_iam_policy_document" "cicd_deployer" {
+  statement {
+    actions = ["s3:*"]
+
+    resources = [
+      "arn:aws:s3:::${var.bucket_name}/*",
+      "arn:aws:s3:::${var.bucket_name}"
+    ]
+  }
+}
+
+resource "aws_iam_user" "cicd_deployer" {
+  name = "cicd_deployer"
+}
+
+resource "aws_iam_access_key" "cicd_deployer" {
+  user = aws_iam_user.cicd_deployer.name
+}
+
+resource "aws_iam_user_policy" "cicd_deployer" {
+  name   = "cicd_deployer_user_policy"
+  user   = aws_iam_user.cicd_deployer.name
+  policy = data.aws_iam_policy_document.cicd_deployer.json
+}
