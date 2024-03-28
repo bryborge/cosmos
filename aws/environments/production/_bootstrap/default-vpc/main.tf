95eeda42deb21d9446e4ab1d58e61a2bbd7c8bef
Bryan Borgeson <bryborge@gmail.com>
2024-03-27 21:11:16 -0700

    bring default subnets on the default vpc under terraform

--- aws/environments/production/_bootstrap/default-vpc/main.tf
+++ aws/environments/production/_bootstrap/default-vpc/main.tf
@@ -20,3 +20,47 @@ provider "aws" {
 module "default_vpc" {
   source = "../../../../modules/default-vpc"
 }
+
+resource "aws_subnet" "default_subnet_1" {
+  vpc_id                  = "vpc-0b5fe43c706844ec9"
+  cidr_block              = "172.31.0.0/20"
+  availability_zone       = "us-west-2c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Default Subnet 1"
+  }
+}
+
+resource "aws_subnet" "default_subnet_2" {
+  vpc_id                  = "vpc-0b5fe43c706844ec9"
+  cidr_block              = "172.31.16.0/20"
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Default Subnet 2"
+  }
+}
+
+resource "aws_subnet" "default_subnet_3" {
+  vpc_id                  = "vpc-0b5fe43c706844ec9"
+  cidr_block              = "172.31.32.0/20"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Default Subnet 3"
+  }
+}
+
+resource "aws_subnet" "default_subnet_4" {
+  vpc_id                  = "vpc-0b5fe43c706844ec9"
+  cidr_block              = "172.31.48.0/20"
+  availability_zone       = "us-west-2d"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Default Subnet 4"
+  }
+}
