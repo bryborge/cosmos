4c3282833ce39068f677291e8085982eb0b4c421
Bryan Borgeson <sonofborge@protonmail.com>
2024-04-26 22:20:37 -0700

    Add: Create CNAME wildcard for subdomains (#42)

--- homelab/terraform/cloudflare/main.tf
+++ homelab/terraform/cloudflare/main.tf
@@ -1,8 +1,16 @@
 # Create "A" record
-resource "cloudflare_record" "bryborge_cloud" {
+resource "cloudflare_record" "bryborge_cloud_a" {
   zone_id = var.cf_zone_id
   name    = "bryborge.cloud"
   value   = var.private_ip # private, because I only want my services accessible locally (or via teleport)
   type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "bryborge_cloud_cname" {
+  zone_id = var.cf_zone_id
+  name    = "*"
+  value   = "bryborge.cloud"
+  type    = "CNAME"
   proxied = false
 }
