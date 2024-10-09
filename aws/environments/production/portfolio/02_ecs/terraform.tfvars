34eb79013247cc6750af061c20ee27f4b5bc5f67
Bryan Borgeson <bryborge@gmail.com>
2024-10-09 10:37:11 -0700

    Remove old ecs-hosting for portfolio; add redirect rules to static s3 bucket site module

--- aws/environments/production/portfolio/02_ecs/terraform.tfvars
+++
@@ -1,6 +0,0 @@
-cluster_name        = "portfolio-app-cluster"
-task_execution_name = "ecsTaskPortfolioAppExecutionRole"
-image_tag           = "portfolio-app:199d4c9e10485bd300e1bfdf61a85e0cd467af43"
-app_name            = "portfolio"
-app_port            = 3000
-service_count       = 2
--- aws/modules/s3-bucket-site/s3.tf
+++ aws/modules/s3-bucket-site/s3.tf
@@ -31,4 +31,27 @@ resource "aws_s3_bucket_website_configuration" "website" {
   error_document {
     key = var.error_html
   }
+
+  routing_rules = <<ROUTING_RULES
+  [
+      {
+          "Condition": {
+              "HttpErrorCodeReturnedEquals": "404"
+          },
+          "Redirect": {
+              "HostName": "${var.domain_name}",
+              "ReplaceKeyPrefixWith": "#!/"
+          }
+      },
+      {
+          "Condition": {
+              "HttpErrorCodeReturnedEquals": "403"
+          },
+          "Redirect": {
+              "HostName": "${var.domain_name}",
+              "ReplaceKeyPrefixWith": "#!/"
+          }
+      }
+  ]
+  ROUTING_RULES
 }
